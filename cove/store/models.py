b1b16a593f86ce0927afede0b15df489c58daaf2
Octowl <alsaff1987@gmail.com>
2016-10-15 15:57:25 +0300

    Get api/items endpoint up and running

--- cove/store/models.py
+++ cove/store/models.py
@@ -8,7 +8,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=3)
     description = models.TextField(blank=True, default='')
-    image = models.ImageField()
+    image = models.ImageField(blank=True, default='')
 
     def __str__(self):
         return self.name
