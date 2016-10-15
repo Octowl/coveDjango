ce9404362ba4797c3f70e388c0000b3eb3b3a810
Octowl <alsaff1987@gmail.com>
2016-10-15 13:40:14 +0300

    Build item model

--- cove/store/models.py
+++ cove/store/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+@python_2_unicode_compatible
+class Item(models.Model):
+    """A user listed item in the store"""
+
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=3)
+    description = models.TextField(blank=True, default='')
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.name
