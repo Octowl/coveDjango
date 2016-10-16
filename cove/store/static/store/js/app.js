3b2daffb7b72d72214907c3ade4eec7ec6d9b45f
Octowl <alsaff1987@gmail.com>
2016-10-16 16:30:59 +0300

    Add container and presentational components for item list

--- cove/store/static/store/js/app.js
+++ cove/store/static/store/js/app.js
@@ -44,10 +44,61 @@ var MainLayout = function(props) {
     )
 };
 
+var Home = function(props) {
+    return (
+        <div className="container">
+            <ItemListContainer />
+        </div>
+    )
+}
+
+var ItemListContainer = React.createClass({
+    getInitialState: function() {
+        return {
+            items: []
+        }
+    },
+
+    componentDidMount: function() {
+        axios.get('api/items/')
+        .then(response => this.setState({items: response.data}))
+    },
+
+    render: function(){
+        return (<ItemList items={this.state.items} />)
+    }
+})
+
+var ItemList = React.createClass({
+    render: function() {
+        return (
+            <div className="row">
+                {this.props.items.map(this.createItem)}
+            </div>
+        );
+    },
+
+    createItem: function(item) {
+        return (
+            <div className="col-sm-4 col-lg-4 col-md-4">
+                <div className="thumbnail">
+                    <img src={item.image.url} alt="" />
+                    <div className="caption">
+                        <h4 className="pull-right">{item.price}KWD</h4>
+                        <h4><Link to="/">{item.name}</Link>
+                        </h4>
+                        <p>{item.description}</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+})
+
 ReactDOM.render((
     <Router history={browserHistory}>
         <Route path="/" component={MainLayout}>
-
+            <IndexRoute component={Home} />
         </Route>
     </Router>
 ), document.getElementById('root'));
